c9543dc48a19d060b7047c883dc2d1a7a05c5d48
unknown <caucuachin@gmail.com>
2022-12-14 23:49:42 +0700

    Merge data

--- stats/defense.py
+++ stats/defense.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from frames import games, info, events
 
-plays = games.query("type == 'play' & event == 'NP'")
+plays = games.query("type == 'play' & event != 'NP'")
 
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
