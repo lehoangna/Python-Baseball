269bdcda4bbb19819fccceaa72e7e5f0f2338a60
unknown <caucuachin@gmail.com>
2022-12-14 18:14:50 +0700

    Group data

--- stats/pitching.py
+++ stats/pitching.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from data import games
 
-plays = games.loc[games['type'] == 'play', :]
+plays = games[games['type'] == 'play']
 strike_outs = plays[plays['event'].str.contains('K')]
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 strike_outs = strike_outs.reset_index(name='strike_outs')
