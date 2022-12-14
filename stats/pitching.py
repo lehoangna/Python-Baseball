8ae10d7e5211d060623d8ef7f2c7321cebf410bd
unknown <caucuachin@gmail.com>
2022-12-14 17:53:08 +0700

    Group data

--- stats/pitching.py
+++ stats/pitching.py
@@ -8,6 +8,6 @@ strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 strike_outs = strike_outs.reset_index(name='strike_outs')
 strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
 
-strike_outs.plot.scatter(x='year', y='strike_outs')
+strike_outs.plot(x='year', y='strike_outs', kind='scatter')
 plt.legend(['Strike Outs'])
 plt.show()
