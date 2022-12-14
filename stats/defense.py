eaa9f8cef4ea468be537c82b6f3651efa82f10ea
unknown <caucuachin@gmail.com>
2022-12-14 23:45:29 +0700

    Merge data

--- stats/defense.py
+++ stats/defense.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from frames import games, info, events
 
-plays = games.query("type == 'play' and event == 'NP'")
+plays = games.query("type == 'play' & event == 'NP'")
 
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
-pa = plays.loc[plays['player'].shift() != plays['player'], :].loc[:, ['year', 'game_id', 'inning', 'team', 'player']]
+pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
 
 pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name='PA')
 
@@ -16,7 +16,7 @@ events = events.unstack().fillna(0).reset_index()
 
 events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
-events = events.rename_axis(None)
+events = events.rename_axis(None, axis='columns')
 
 events_plus_pa = pd.merge(events, pa, how= 'outer', left_on=['year', 'game_id', 'team'], right_on= ['year', 'game_id', 'team'])
 
